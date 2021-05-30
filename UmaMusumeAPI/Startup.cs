9ec606de1ca3ecf7e0cfb6554ab40d15869db9a5
SimpleSandman <simple.sandman@gmail.com>
2021-05-30 14:29:05 -0500

    Remove unused namespace

--- UmaMusumeAPI/Startup.cs
+++ UmaMusumeAPI/Startup.cs
@@ -4,7 +4,6 @@ using System.Reflection;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
