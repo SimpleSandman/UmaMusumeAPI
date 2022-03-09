8b3649c7491fbd554c82e8bef5ad6bff3c1d6d5c
SimpleSandman <simple.sandman@gmail.com>
2022-03-09 01:06:26 -0600

    Added response validation & a few more endpoints for testing

--- UmaMusumeAPI.Test/Fixtures/IntegrationTest.cs
+++ UmaMusumeAPI.Test/Fixtures/IntegrationTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -23,14 +24,46 @@ namespace UmaMusumeAPI.Test.Fixtures
         /// Get the deserialized response from the API request
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="endpoint">The actual request for API testing which is already prefixed with the <see cref="string"/>, /api/</param>
+        /// <param name="endpointUrl">The actual request for API testing which is already prefixed with the <see cref="string"/>, /api/</param>
         /// <param name="hasSingleReturn">Will the returning object contain a list of objects?</param>
         /// <returns></returns>
-        protected async Task<object?> ReturnResponseAsync<T>(string endpoint, bool hasSingleReturn)
+        protected async Task<object?> ReturnResponseAsync<T>(string endpointUrl, bool hasSingleReturn)
         {
             return hasSingleReturn
-                ? await _client.GetAndDeserialize<T>(endpoint)
-                : await _client.GetAndDeserialize<IEnumerable<T>>(endpoint);
+                ? await _client.GetAndDeserialize<T>(endpointUrl)
+                : await _client.GetAndDeserialize<IEnumerable<T>>(endpointUrl);
+        }
+
+        /// <summary>
+        /// Check the API response for the expected outcome
+        /// </summary>
+        /// <param name="response">The API response in question</param>
+        /// <param name="hasSingleReturn">Validate non-enumerable objects</param>
+        /// <param name="knownEmptyTable">Check if response is empty as expected</param>
+        protected static void ValidateResponse<T>(object? response, bool hasSingleReturn, bool knownEmptyTable = false)
+        {
+            if (response == null)
+            {
+                return; // don't assert anything
+            }
+
+            if (hasSingleReturn)
+            {
+                T singleResponse = (T)response;
+                Assert.NotNull(singleResponse);
+            }
+            else
+            {
+                IEnumerable<T> listResponse = (IEnumerable<T>)response;
+                if (knownEmptyTable)
+                {
+                    Assert.True(!listResponse.Any());
+                }
+                else
+                {
+                    Assert.True(listResponse.Any());
+                }
+            }
         }
     }
 }
--- UmaMusumeAPI.Test/TableTests.cs
+++ UmaMusumeAPI.Test/TableTests.cs
@@ -1,16 +1,18 @@
 ﻿using System.Threading.Tasks;
 
-using FluentAssertions;
-
 using Microsoft.AspNetCore.Mvc.Testing;
 
 using UmaMusumeAPI.Models.Tables;
+
 using UmaMusumeAPI.Test.Fixtures;
 
 using Xunit;
 
 namespace UmaMusumeAPI.Test
 {
+    /// <summary>
+    /// Testing every actual database table from the API
+    /// </summary>
     public class TableTests : IntegrationTest
     {
         public TableTests(WebApplicationFactory<Startup> api) : base(api) { }
@@ -19,10 +21,90 @@ namespace UmaMusumeAPI.Test
         [Trait("EndpointType", "Raw")]
         [InlineData("AnnounceCharacter", false)]
         [InlineData("AnnounceCharacter/101", true)]
-        public async Task GetAnnounceCharacter(string endpoint, bool hasSingleReturn)
+        public async Task GetAnnounceCharacter(string endpointUrl, bool hasSingleReturn)
         {
-            object? response = await ReturnResponseAsync<AnnounceCharacter>(endpoint, hasSingleReturn);
-            response.Should().NotBeNull();
+            object? response = await ReturnResponseAsync<AnnounceCharacter>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AnnounceCharacter>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AnnounceData", false)]
+        [InlineData("AnnounceData/30004", true)]
+        public async Task GetAnnounceData(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AnnounceData>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AnnounceData>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AnnounceEvent", false)]
+        [InlineData("AnnounceEvent/1", true)]
+        public async Task GetAnnounceEvent(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AnnounceEvent>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AnnounceEvent>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AnnounceSupportCard", false)]
+        [InlineData("AnnounceSupportCard/501", true)]
+        public async Task GetAnnounceSupportCard(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AnnounceSupportCard>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AnnounceSupportCard>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AudienceData", false)]
+        [InlineData("AudienceData/1", true)]
+        public async Task GetAudienceData(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AudienceData>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AudienceData>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AudienceDressColorSet", false)]
+        [InlineData("AudienceDressColorSet/1", true)]
+        public async Task GetAudienceDressColorSet(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AudienceDressColorSet>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AudienceDressColorSet>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AudienceHairColorSet", false)]
+        [InlineData("AudienceHairColorSet/1", true)]
+        public async Task GetAudienceHairColorSet(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AudienceHairColorSet>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AudienceHairColorSet>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AudienceImpostor", false)]
+        [InlineData("AudienceImpostor/110", false)]
+        public async Task GetAudienceImpostor(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AudienceImpostor>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AudienceImpostor>(response, hasSingleReturn);
+        }
+
+        [Theory]
+        [Trait("EndpointType", "Raw")]
+        [InlineData("AudienceImpostorColorSet", false)]
+        [InlineData("AudienceImpostorColorSet/1", false)]
+        public async Task GetAudienceImpostorColorSet(string endpointUrl, bool hasSingleReturn)
+        {
+            object? response = await ReturnResponseAsync<AudienceImpostorColorSet>(endpointUrl, hasSingleReturn);
+            ValidateResponse<AudienceImpostorColorSet>(response, hasSingleReturn, true);
         }
     }
 }
